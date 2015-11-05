ed2bedeaf54a2eae8d7a7c4ea809f3355584e1f5
richj0985 <richj0985@HRH-IC0027343.wrdsb.ca>
2015-11-05 13:18:13 -0500

    School Changes 1 (Initial Commit)

---
+++ src/Question1.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author richj0985
+ */
+public class Question1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create a scanner for input
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the radius ");
+        double radius = input.nextDouble();
+        
+        public static double circleArea(double radius) {
+            double answer = radius * radius * 3.14;
+            System.out.println(double answer);
+        }
+    }
+}
