672aecc2f814357adef7907caec14f508460aaba
hadik9595 <hadik9595@HRH-IC0027323.wrdsb.ca>
2015-11-05 13:21:12 -0500

    Let's see...

---
+++ src/A6Q1.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hadik9595
+ */
+public class A6Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Please enter the radius");
+        double radius = in.nextInt();
+                double pi = 3.14159265359;
+                
+        
+        
+        System.out.println(area);
+    }
+    public static double circleArea(double radius, double pi){
+    
+    double area = (pi)*(radius);
+    return area;
+            
+}
+}
