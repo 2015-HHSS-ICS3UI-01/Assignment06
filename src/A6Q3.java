598b7c11ff842686f4fecb932413d60f6ccfb4cf
Administrator <Administrator@n-9c4c592af9fe4>
2015-11-09 07:28:34 -0500

    DoneQ3

--- src/A6Q3.java
+++ src/A6Q3.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author hadik9595
@@ -16,19 +15,33 @@ public class A6Q3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner in = new Scanner (System.in);
-        
+        // generate scanner
+        Scanner in = new Scanner(System.in);
+        // Asks for number to divide
         System.out.println("Enter a number");
         int number = in.nextInt();
-        
-        System.out.println("factors (" +number+ ")whould print out the numbers" );
+        // Prints out AND EXECUTES main method factors
+        System.out.println("factors (" + number + ") would print out the numbers ");
+        Factors(number);
     }
-    public static int Factors(int number){
-        
-       int answer = (number/2);
-            
-        return answer;
-        
+
+    public static void Factors(int number) {
+        // Intialized check as number basically
+        int Check = number;
+        // i starts at 1 and adds on until it equals = number
+        int i = 1;
+        // while loop that keeps going until i becomes filled
+        while (i <= Check) {
+            // Most important part of the code.. It looks if its remainder is 0 then prints it.
+            if (Check % i == 0) {
+                //prints any evenly divisible numbers.
+                System.out.println(i + " is a factor");
+
+            }
+            // Adds 1 until i is no longer is divisible evenly and reaches the original number.
+            i++;
+
+        }
+
     }
 }
-
