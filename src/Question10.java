c8d9e7e4dc83abc2a2013cd1302bcd5663d692d2
richj0985 <richj0985@HRH-IC0027343.wrdsb.ca>
2015-11-10 14:23:24 -0500

    School Changes 4

---
+++ src/Question10.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author richj0985
+ */
+public class Question10 {
+
+    public static boolean sameDashes(String setOne, String setTwo) {
+        
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create a scanner for input
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter the first set of letters / digits: ");
+        String setOne = input.nextLine();
+        System.out.println("Enter the second set of letters / digits: ");
+        String setTwo = input.nextLine();
+    }
+}
--- src/Question9.java
+++ src/Question9.java
@@ -13,12 +13,41 @@ import java.util.Scanner;
  */
 public class Question9 {
 
+    public static boolean allDigitsOdd(int number) {
+        boolean odd = false;
+        int lastDigit;
+        while(odd == false){
+            lastDigit = number % 2;
+            if(lastDigit != 0){
+                number = (number - lastDigit) / 10;
+                if(number == 0){
+                    break;
+                }
+            } else{
+                odd = true;
+            }
+        }
+        return odd;
+        
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // create a scanner for input
         Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter a number: ");
+        int number = input.nextInt();
+        
+        boolean answer = allDigitsOdd(number);
+        
+        if(answer == false){
+            System.out.println("The number is all odd");
+        } else{
+            System.out.println("The number is not all odd");
+        }
     }
     
 }
