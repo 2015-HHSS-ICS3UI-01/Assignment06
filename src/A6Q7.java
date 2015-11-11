55d798784bd9857b1bceac6815a992ae4bb75e22
butlj9437 <butlj9437@HRH-IC0027341.wrdsb.ca>
2015-11-11 13:26:30 -0500

    started Q6-7

---
+++ src/A6Q7.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author butlj9437
+ */
+public class A6Q7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static int firstDigit(int num){
+        for(int mult = 0; )
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter a number.");
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        int dig = firstDigit(num);
+        System.out.println(dig);
+    }
+}
