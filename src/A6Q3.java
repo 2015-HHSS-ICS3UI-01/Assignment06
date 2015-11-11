042425a56cac593c46b13b65d63ff835fe3a7e59
sevcm7279 <sevcm7279@HRH-IC0027347.wrdsb.ca>
2015-11-11 13:40:43 -0500

    l

---
+++ src/A6Q3.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sevcm7279
+ */
+public class A6Q3 {
+    
+    public static double factors (double n){
+    
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
