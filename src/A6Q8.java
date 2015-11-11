7f4a8abd11578f4aebd7e4487c049748eb519d0b
hadik9595 <hadik9595@HRH-IC0027323.wrdsb.ca>
2015-11-11 13:41:04 -0500

    Question8

---
+++ src/A6Q8.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hadik9595
+ */
+public class A6Q8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Enter the month, and the day please.");
+        int month = in.nextInt();
+        int day = in.nextInt();
+    }
+}
