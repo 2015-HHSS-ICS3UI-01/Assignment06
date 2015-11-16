31f4ea50d14a13890f034cea1a9a78d9811244a3
stahc1596 <stahc1596@HRH-IC0027346.wrdsb.ca>
2015-11-16 14:24:03 -0500

    Cameron S.

---
+++ src/Question5.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author stahc1596
+ */
+public class Question5 {
+
+    public static void random(int i){
+        for(int f = i; f > 0; f--){
+            
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
