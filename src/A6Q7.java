0d0b22ee868c41e8e9094d69a003f9059d5796f8
watsk8668 <watsk8668@HRH-IC0027333.wrdsb.ca>
2015-11-17 12:55:47 -0500

    A6 Q1,Q2,Q3,Q4,Q5,Q6 done Q7 almost done

--- src/A6Q7.java
+++ src/A6Q7.java
@@ -8,21 +8,28 @@
  * @author watsk8668
  */
 public class A6Q7 {
+
     public static int firstDigit(int number) {
         int first = number;
-        if(number >= 0)
+        while (number >= 0) {
+            if (first <= 9) {
+                break;
+            }
             first = number / 10;
-        if(number < 0){
+        }
+
+        while (number < 0) {
             first = number / 10 * -1;
         }
         System.out.println(first);
         return first;
     }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         //
-        firstDigit(56); 
+        firstDigit(567);
     }
 }
