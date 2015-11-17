05426be3db157a97afb9986f2d964663d149e5f9
sevcm7279 <sevcm7279@HRH-IC0027347.wrdsb.ca>
2015-11-17 12:40:22 -0500

    l

--- src/A6Q3.java
+++ src/A6Q3.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -11,15 +14,37 @@ public class A6Q3 {
     
     public static void factors (int n){
     
-        for (int factors = 1; )
+        //create a for loop to check every number between 1 and the number
+        for (int factors = 1; factors <= n; factors = factors + 1){
+            
+            //if there are no remainders...
+            if (n%factors == 0){
+                
+            //...tell user those factors    
+                System.out.print(factors+",");
+            }
         
-        
-    }
+    }}
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         
+        //make a new scanner
+        
+        Scanner input = new Scanner(System.in);
+        
+        //tell user to enter a number to find the factors for
+        
+        System.out.println("Please enter a number.");
+        
+        //store that number in an int
+        
+        int n = input.nextInt();
+        
+        //activate the method to determine the factors
+        
+        factors (n);
     }
 }
