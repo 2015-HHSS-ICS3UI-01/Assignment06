e96266780646f628f3d5ec6e02424d9e87c06858
stahc1596 <stahc1596@HRH-IC0027346.wrdsb.ca>
2015-11-18 14:25:11 -0500

    Cameron S.

---
+++ src/Question8.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author stahc1596
+ */
+public class Question8 {
+    
+    public static int season(int m, int d){
+        m = m*100;
+        int s = m + d;
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("Please input a month.");
+        int month = input.nextInt();
+        System.out.println("Please input a day.");
+        int day = input.nextInt();
+        int answer = season(month, day);
+        if (answer >= 1216 || answer <= 315){
+            System.out.println("Winter");
+        }else if(answer >= 316 && answer <= 615){
+            System.out.println("Spring");
+        }else if(answer >= 616 && answer <=915){
+            System.out.println("Summer");
+        }else if()
+        
+    }
+}
