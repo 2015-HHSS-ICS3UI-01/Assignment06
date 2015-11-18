188a7f0f03b4a689bf55c7a82514ec7095a46e31
sevcm7279 <sevcm7279@HRH-IC0027347.wrdsb.ca>
2015-11-18 14:08:36 -0500

    l

--- src/A6Q5.java
+++ src/A6Q5.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -11,13 +14,37 @@ public class A6Q5 {
 
     public static void chaotic (int n){
         
-        for ()
         
-    }
+        
+        for (int lines = 0; lines <= n; lines = lines ++){
+            
+            int randomNumber = (int)(Math.random()*100) + 1;
+            
+        if (randomNumber >= 0 && randomNumber <= 19){
+            System.out.println("*");
+        }else if (randomNumber >= 20 && randomNumber <= 39){
+            System.out.println("**");
+        }else if (randomNumber >= 40 && randomNumber <= 59){
+            System.out.println("***");
+        }else if (randomNumber >= 60 && randomNumber <= 79){
+            System.out.println("****");
+        }else if (randomNumber >= 80 && randomNumber <= 100){
+            System.out.println("*****");
+        }
+        
+    }}
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner input = new Scanner (System.in);
+        
+        System.out.println("Please enter a number");
+        
+        int n = input.nextInt();
+        
+        chaotic (n);
+     
     }
 }
