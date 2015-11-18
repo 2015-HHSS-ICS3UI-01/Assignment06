8887f8f7a7d944ab7dbd2eb4cf4848a77b6c04db
hadik9595 <hadik9595@HRH-IC0027323.wrdsb.ca>
2015-11-18 14:06:34 -0500

    Done all questions!

--- src/A6Q10.java
+++ src/A6Q10.java
@@ -1,15 +1,15 @@
 
+
 import java.util.Scanner;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 
 /**
  *
- * @author Administrator
+ * @author hadik9595
  */
 public class A6Q10 {
 
@@ -17,42 +17,37 @@ public class A6Q10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        
+        //generates scanner
+       Scanner in = new Scanner(System.in);
+       // Asks for 2 desired strings
         System.out.println("Please enter 2 desired phrases");
         String first = in.nextLine();
         String second = in.nextLine();
-        
-        boolean answer = sameDashes(first,second);
+        // A boolean type method that executes 
+        boolean answer = sameDashes(first, second);
+        // Prints out if it's true or false
         System.out.println(answer);
-        
-    }
-    public static boolean sameDashes(String first, String second){
-        while(true){
-         int dashFound = first.indexOf("-");
-         int dashFound2 = second.indexOf("-");
-          
-               String first1 = first.substring(0, dashFound);
-            String first2 = second.substring(0,dashFound2);
-            
-             int numChars = first1.length();
-             int numChars2 = first2.length();
-             
-             
-             first = first1.replace(first1, "");
-             second = first2.replace(first2, "");
 
-             int answer = (numChars2 - numChars);
-           
-             if(answer != 0){
-                 return false;
-             }
-             if (numChars != 0 && numChars2 != 0){
-                 return true;
-             }
-            return true;
-            
-        } 
     }
-    
+    // the method for finding the dashes
+     public static boolean sameDashes(String first, String second) {
+         // takes the length of first word, which is really all we need
+          int numChars = first.length();
+          // A counted loop, starting at 0 and keeps adding each letter it scans
+             for ( int count = 0; count < numChars;){
+                 
+                 // takes the corresponding letter (e.g 2 is a)
+                 char first1 = first.charAt(count);
+                char second1 = second.charAt(count);
+     
+                // if the character in string 1 has a dash and string doesn't, return false, vise versa
+                 if (second1 != '-' && first1 == '-' || second1 == '-' && first1 != '-') {
+                return false; 
+             }
+                 //adds one to count which goes on to next letter
+                count++; 
+     }// returns true otherwise
+        return true;
+    }
 }
+
