fb6c8559719d0dfdc71ac256dd894f035d17edb5
stahc1596 <stahc1596@HRH-IC0027346.wrdsb.ca>
2015-11-19 13:16:09 -0500

    Cameron S.

---
+++ src/Question9.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author stahc1596
+ */
+public class Question9 {
+
+    public static void allDigitsOdd(int digit){
+        while 
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        int number = input.nextInt();
+        
+    }
+}
