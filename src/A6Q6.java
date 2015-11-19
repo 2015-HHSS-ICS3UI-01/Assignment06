4395ce0d58646c86b72036ee346da0afaf772d54
sevcm7279 <sevcm7279@HRH-IC0027347.wrdsb.ca>
2015-11-19 12:44:09 -0500

    l

--- src/A6Q6.java
+++ src/A6Q6.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -9,16 +12,37 @@
  */
 public class A6Q6 {
     
-    public static int lastDigit (n){
+    public static int lastDigit (int n){
         
+        //find the remainder when the number is divided by ten
+        int i = n%10;
         
-      
+        //return that number
+        return i;
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    
+        //make a new Scanner
+        Scanner input = new Scanner (System.in);
+        
+        //tell the user to enter a number
+        System.out.println("Please enter a number.");
+        
+        //store the number
+        int n = input.nextInt();
+        
+        //activate the method and store the returned value as a new int
+        int lastDigit = lastDigit (n);
+        
+        //tell user the last digit
+        System.out.println("The last digit of the number that was entered is " + lastDigit);
+        
+        //close the scanner
+        input.close();
+        
     }
 }
