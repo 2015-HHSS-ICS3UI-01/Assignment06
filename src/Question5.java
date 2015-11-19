dc356a8ac6d012bf1b99f22fa544a3ac9fbdc507
stahc1596 <stahc1596@HRH-IC0027346.wrdsb.ca>
2015-11-19 13:15:38 -0500

    Cameron S.

--- src/Question5.java
+++ src/Question5.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -9,12 +12,18 @@
  */
 public class Question5 {
 
-    public static void random(int i){
-        for(int f = i; f > 0; f--){
-            
+    public static void chaotic(int i){
+        for(int o = i; o > 0; o--){
+        int e = (int)(Math.random()*5)+1;
+        for(int f = e; f > 0; f--){
+            System.out.print("*");
         }
+            System.out.println();}
     }
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner (System.in);
+        System.out.println("Please enter a number.");
+        int n = input.nextInt();
+        chaotic(n);
     }
 }
