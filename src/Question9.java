36daf9e895fa1be9e99efa954d8e743aa9d10498
stahc1596 <stahc1596@HRH-IC0027346.wrdsb.ca>
2015-11-20 14:24:23 -0500

    Cameron S.

--- src/Question9.java
+++ src/Question9.java
@@ -12,12 +12,31 @@ import java.util.Scanner;
  */
 public class Question9 {
 
-    public static void allDigitsOdd(int digit){
-        while 
+    public static int allDigitsOdd(int digit){
+        int n = 0;
+            digit = digit%10;
+            if(digit == 0 || digit == 2 || digit == 4 || digit == 6 || digit == 8){
+                n = 1;
+            }else{
+                n = 0;
+            }return n;
     }
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
+        System.out.println("Please enter a number:");
         int number = input.nextInt();
+        while (true){
+            int choice = allDigitsOdd(number);
+            number = number/10;
+            if (choice == 1){
+                System.out.println("False");
+                break;
+           }else if (number < 10){
+                System.out.println("True");
+                break;
+           }
+        }
+        }
         
     }
-}
+
