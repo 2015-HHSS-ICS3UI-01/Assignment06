110d6d026c0f04471554f7f638bce7257956dc36
stahc1596 <stahc1596@HRH-IC0027346.wrdsb.ca>
2015-11-23 13:09:09 -0500

    Cameron S.

--- src/Test.java
+++ src/Test.java
@@ -13,6 +13,8 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        String hello = "dinosaur";
+        String sub = hello.substring(4, 5);
+        System.out.println(sub);
     }
 }
