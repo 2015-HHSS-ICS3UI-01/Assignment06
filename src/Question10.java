838dfdffc346ce24b148a14d3b30bdd93a2b4d4b
stahc1596 <stahc1596@HRH-IC0027346.wrdsb.ca>
2015-11-23 13:02:49 -0500

    Cameron S.

--- src/Question10.java
+++ src/Question10.java
@@ -11,14 +11,24 @@ import java.util.Scanner;
  * @author stahc1596
  */
 public class Question10 {
-
-    public static sameDashes(String string1, String string2){
+    
+    public static void sameDashes(String string1, String string2){
         int time = 0;
+        int time2 = 1;
         int length = string1.length();
-        while (time >= length){
-            int first = string1.indexOf("-");
-            int first2 = string2.indexOf("-");
-            ;
+        int length2 = string2.length();
+        while (time <= length || time <= length2){
+            String sub1 = string1.substring(time, time2);
+            String sub2 = string2.substring(time, time2);
+            time = time + 1;
+            if (sub1.equalsIgnoreCase("-") && !sub2.equalsIgnoreCase("-") || !sub1.equalsIgnoreCase("-") && sub2.equalsIgnoreCase("-")){
+                System.out.println("False");
+                break;
+            }
+            
+        }
+        if (time > length){
+            System.out.println("True");
         }
     }
     public static void main(String[] args) {
