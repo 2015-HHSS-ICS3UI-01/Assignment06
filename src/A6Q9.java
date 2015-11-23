c3b42c9be350a2d42bdbca819dd485f99c54e82d
sevcm7279 <sevcm7279@HRH-IC0027347.wrdsb.ca>
2015-11-23 12:49:11 -0500

    l

--- src/A6Q9.java
+++ src/A6Q9.java
@@ -15,24 +15,30 @@ public class A6Q9 {
     public static void allDigitsOdd (int e){
             
         //determine if all of the digits are odd
+        
+        //start odd didgit count at 0
             int t = 0;
             
             while (e > 0){
-                
+        //find the first number        
                 int i = e%10;
-                
+        //if it is odd        
                 if (i == 1 || i == 3 || i == 5 | i == 7 || i == 9){
+        //divide by ten to move to the next digit            
                     e = e/10;
+        //add a count to show that that is an odd number            
                     t = t +1;
+        //if the number becomes zero or below, break            
                         if (e <= 0){
                             break;}
-                        
+        //if the digit found is an even number, make the count zero and break                
                 }else{ t = 0; break;}
                 
             }
+        //if the count of odd numbers is greater than zero then it is true
                 if ( t > 0){
                     System.out.println("True");
-                    
+        //if the count of odd numbers is zero then it is false            
                 }else if (t == 0){
                     System.out.println("False");
            }
