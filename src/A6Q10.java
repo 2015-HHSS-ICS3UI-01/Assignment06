f395e452e6287e686e65d89275d3585324affedd
sevcm7279 <sevcm7279@HRH-IC0027347.wrdsb.ca>
2015-11-23 13:07:27 -0500

    l

---
+++ src/A6Q10.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sevcm7279
+ */
+public class A6Q10 {
+    
+    public static void sameDashes1 (String firstString){
+        
+        
+    }
+    public static void sameDashes2 (String secondString){
+        
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // make a scanner
+        Scanner input = new Scanner (System.in);
+        
+        //tell the user to enter the first string
+        System.out.println("Please enter the first string.");
+        
+        //store the first string in a variable
+        String firstString = input.nextLine();
+        
+        //tell the user to input  the second string
+        System.out.println("please the enter the second string.");
+        
+        //store the second d=string in a variable]
+        String secondString = input.nextLine();
+        
+        
+    }
+}
