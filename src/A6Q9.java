b81cc57a345f237f20be1cd15f8b3aae5b01d4f3
sevcm7279 <sevcm7279@HRH-IC0027347.wrdsb.ca>
2015-11-23 12:44:54 -0500

    l

--- src/A6Q9.java
+++ src/A6Q9.java
@@ -15,23 +15,28 @@ public class A6Q9 {
     public static void allDigitsOdd (int e){
             
         //determine if all of the digits are odd
-        int t = 0;
+            int t = 0;
             
             while (e > 0){
+                
                 int i = e%10;
+                
                 if (i == 1 || i == 3 || i == 5 | i == 7 || i == 9){
                     e = e/10;
-                    t = t ++;
-                }else{ t = 0; break;
-            }}
+                    t = t +1;
+                        if (e <= 0){
+                            break;}
+                        
+                }else{ t = 0; break;}
+                
+            }
                 if ( t > 0){
                     System.out.println("True");
+                    
                 }else if (t == 0){
                     System.out.println("False");
            }
-            
-        
-            }
+    }
         
         
     
