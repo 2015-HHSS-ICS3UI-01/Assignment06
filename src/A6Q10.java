a1bf89ad6091f03414ec089c586b1a056a5695f9
watsk8668 <watsk8668@HRH-IC0027333.wrdsb.ca>
2015-11-24 14:24:16 -0500

    A6 Q1,Q2,Q3,Q4,Q5,Q6,Q8 done Q7 almost done

--- src/A6Q10.java
+++ src/A6Q10.java
@@ -8,11 +8,15 @@
  * @author watsk8668
  */
 public class A6Q10 {
-
+        public static String sameDashes(String string1, String string2){
+            
+            return
+        }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //
+        sameDashes("hel-lo", "123-45");
     }
 }
--- src/A6Q9.java
+++ src/A6Q9.java
@@ -8,11 +8,14 @@
  * @author watsk8668
  */
 public class A6Q9 {
-
+        public static String allDigitsOdd(int number, String trueFalse){
+            
+            return trueFalse;
+        }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //
     }
 }
