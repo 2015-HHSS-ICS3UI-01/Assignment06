87c2ec02aa8607241fb62a3c48b50160b29b0cda
stahc1596 <stahc1596@HRH-IC0027346.wrdsb.ca>
2015-11-24 14:00:52 -0500

    Cameron S.

--- src/Question5.java
+++ src/Question5.java
@@ -13,7 +13,10 @@ import java.util.Scanner;
 public class Question5 {
 
     public static void chaotic(int i){
+        //There are the same amount of lines as the users inputed number.
         for(int o = i; o > 0; o--){
+            //The program finds a number between 1 and 5 and prints that many stars
+            //out on that line.
         int e = (int)(Math.random()*5)+1;
         for(int f = e; f > 0; f--){
             System.out.print("*");
@@ -23,7 +26,10 @@ public class Question5 {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
         System.out.println("Please enter a number.");
+        //The user inputs an amount of lines that the program is going to use.
         int n = input.nextInt();
+        //The method that creates the lines and stars is called chaotic and is called
+        //upon here.
         chaotic(n);
     }
 }
