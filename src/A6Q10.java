bc65ff9a6c91918fd49086bb1b6b9a9707105d38
sevcm7279 <sevcm7279@HRH-IC0027347.wrdsb.ca>
2015-11-25 13:19:08 -0500

    l

--- src/A6Q10.java
+++ src/A6Q10.java
@@ -5,41 +5,51 @@ import java.util.Scanner;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author sevcm7279
  */
 public class A6Q10 {
-    
-    public static void sameDashes1 (String firstString){
+
+    public static void sameDashes(String firstString) {
+
+        //make a scanner
+        Scanner input = new Scanner(System.in);
+        
+        //ask the user to input the second string
+        System.out.println("please the enter the second string.");
+
+        //store the second string in a variable
+        String secondString = input.nextLine();
+        
+        //find the location of all of the "-" in the first string
+        int dash = firstString.indexOf("-");
+        int dash2 =secondString.indexOf("-");
+        System.out.println(dash);
         
         
-    }
-    public static void sameDashes2 (String secondString){
         
         
+        
+        
+
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // make a scanner
-        Scanner input = new Scanner (System.in);
-        
+        //make a scanner
+        Scanner input = new Scanner(System.in);
+
         //tell the user to enter the first string
         System.out.println("Please enter the first string.");
-        
+
         //store the first string in a variable
         String firstString = input.nextLine();
         
-        //tell the user to input  the second string
-        System.out.println("please the enter the second string.");
-        
-        //store the second d=string in a variable]
-        String secondString = input.nextLine();
-        
-        
+        sameDashes (firstString);
+
+
     }
 }
