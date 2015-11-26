8e05478ce91adffcd3b8c53f31125ce701c20f90
sevcm7279 <sevcm7279@HRH-IC0027347.wrdsb.ca>
2015-11-26 14:24:56 -0500

    l

--- src/A6Q10.java
+++ src/A6Q10.java
@@ -11,27 +11,39 @@ import java.util.Scanner;
  */
 public class A6Q10 {
 
-    public static void sameDashes(String firstString) {
+    public static void sameDashes(String firstString, String secondString) {
 
-        //make a scanner
-        Scanner input = new Scanner(System.in);
-        
-        //ask the user to input the second string
-        System.out.println("please the enter the second string.");
 
-        //store the second string in a variable
-        String secondString = input.nextLine();
-        
+
         //find the location of all of the "-" in the first string
-        int dash = firstString.indexOf("-");
-        int dash2 =secondString.indexOf("-");
-        System.out.println(dash);
-        
-        
-        
-        
-        
-        
+        while (true) {
+
+
+            //determine the loctaion of the first dash in both strings   
+            int dash = firstString.indexOf("-");
+            int dash2 = secondString.indexOf("-");
+
+            //if the dashes are in the same location    
+            if (dash == dash2) {
+
+                //cut off the string from the start to the dashes   
+                firstString = firstString.substring(0, dash + 1);
+                secondString = secondString.substring(0, dash2 + 1);
+
+                //if no other dashes can be found tell user it is true
+                if (dash == -1 && dash2 == -1) {
+                    System.out.println("True.");
+                    break;
+                }
+
+                //if the dashes are not in the same location    
+            } else if (dash != dash2) {
+                System.out.println("False.");
+
+                break;
+
+            }
+        }
 
     }
 
@@ -47,8 +59,18 @@ public class A6Q10 {
 
         //store the first string in a variable
         String firstString = input.nextLine();
-        
-        sameDashes (firstString);
+
+        //ask the user to input the second string
+        System.out.println("Please the enter the second string.");
+
+        //store the second string in a variable
+        String secondString = input.nextLine();
+
+        //activate the method
+        sameDashes(firstString, secondString);
+
+        //close the scanner
+        input.close();
 
 
     }
