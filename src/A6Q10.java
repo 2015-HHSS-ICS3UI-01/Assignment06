efb79c9bb43dc3d598de89f64738e0f516ead67a
watsk8668 <watsk8668@HRH-IC0027333.wrdsb.ca>
2015-11-26 13:34:45 -0500

    A6 Q1,Q2,Q3,Q4,Q5,Q6,Q8,Q9 done Q7 almost done

--- src/A6Q10.java
+++ src/A6Q10.java
@@ -8,15 +8,25 @@
  * @author watsk8668
  */
 public class A6Q10 {
-        public static String sameDashes(String string1, String string2){
-            
-            return
+
+    public static String sameDashes(String string1, String string2) {
+        int dashes = string1.indexOf("-");
+        int dashes2 = string2.indexOf("-");
+
+        if (dashes == dashes2) {
+            System.out.println("True");
+            return "True";
+        } else {
+            System.out.println("False");
+            return "False";
         }
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         //
-        sameDashes("hel-lo", "123-45");
+        sameDashes("abc", "9.8");
     }
 }
