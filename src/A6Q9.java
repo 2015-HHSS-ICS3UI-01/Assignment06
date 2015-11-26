74f0042ac37af6595210fda4762cd1389fcf9fc7
watsk8668 <watsk8668@HRH-IC0027333.wrdsb.ca>
2015-11-26 14:24:12 -0500

    A6 Q1,Q2,Q3,Q4,Q5,Q6,Q8,Q10 done Q7,Q9 almost done

--- src/A6Q9.java
+++ src/A6Q9.java
@@ -8,14 +8,27 @@
  * @author watsk8668
  */
 public class A6Q9 {
-        public static String allDigitsOdd(int number, String trueFalse){
-            
-            return trueFalse;
+
+    public static String allDigitsOdd(int number) {
+        String trueFalse = "tF";
+        if(number >  1) {
+        int digits = number / 10;
+        } else {
+            if(number == 0 || number == 2 || number == 4 || number ==6 || number == 8){
+                trueFalse = "False";
+        } else {
+                trueFalse = "True";
+            }
         }
+        System.out.println(trueFalse);
+        return trueFalse;
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         //
+        allDigitsOdd(4);
     }
 }
