62b2d46272e02b2a9aeb9f2253f52df8cd837252
watsk8668 <watsk8668@HRH-IC0027333.wrdsb.ca>
2015-11-27 13:18:28 -0500

    A6 Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8,Q10 done Q9 almost done

--- src/A6Q9.java
+++ src/A6Q9.java
@@ -11,24 +11,32 @@ public class A6Q9 {
 
     public static String allDigitsOdd(int number) {
         String trueFalse = "tF";
-        if(number >  1) {
-        int digits = number / 10;
-        } else {
-            if(number == 0 || number == 2 || number == 4 || number ==6 || number == 8){
+        int digits = number;
+        int divide = 10;
+        while (digits > 9) {
+            if (digits == 0 || digits == 2 || digits == 4 || digits == 6 || digits == 8) {
                 trueFalse = "False";
-        } else {
+                break;
+            }
+            digits = number / divide;
+            divide = divide * 10;
+        }
+            if (digits == 0 || digits == 2 || digits == 4 || digits == 6 || digits == 8) {
+                trueFalse = "False";
+            } else {
                 trueFalse = "True";
             }
-        }
-        System.out.println(trueFalse);
-        return trueFalse;
+            System.out.println(trueFalse);
+            return trueFalse;
     }
 
-    /**
-     * @param args the command line arguments
-     */
+        /**
+         * @param args the command line arguments
+         */
+    
+
     public static void main(String[] args) {
         //
-        allDigitsOdd(4);
+        allDigitsOdd(1675);
     }
 }
