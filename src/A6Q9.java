56bc8cad22a79b6873bfd74644c32706a69b2fca
lalim9800 <lalim9800@HRH-IC0027329.wrdsb.ca>
2015-11-27 13:20:24 -0500

    a6.....

---
+++ src/A6Q9.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author lalim9800
+ */
+public class A6Q9 {
+
+    public static boolean alldigitsodd(int number) {
+
+        if (number == 9) {
+             || number == 7
+                    || number == 5
+                    || number == 3
+                    || number == 1
+       
+
+        }
+
+        /**
+         * @param args the command line arguments
+         */
+    
+
+    public static void main(String[] args) {
+        //new scanner
+        Scanner input = new Scanner(System.in);
+        //ask user a number 
+        System.out.println("Please enter a number ");
+        //allow user to enter number 
+        int number = input.nextInt();
+
+        boolean Num = alldigitsodd(number);
+
+
+    }
+}
