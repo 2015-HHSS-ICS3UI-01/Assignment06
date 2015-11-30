1cce1a36669c04cdcf3c9f0265306ad73688d130
lalim9800 <lalim9800@HRH-IC0027329.wrdsb.ca>
2015-11-30 14:24:00 -0500

    a6.....

--- src/A6Q8.java
+++ src/A6Q8.java
@@ -12,40 +12,50 @@ import java.util.Scanner;
 public class A6Q8 {
 
     public static String season(int month, int day) {
+        //set default season to winter 
         String season = "winter";
 
 
 
 
-        if (month < 4) {
+        if (month < 4) { //if month is less than 4 season is winter 
             season = "winter";
 
-        }
-        
-        
-        else  if (month == 3) {
+        } else if (month == 3) { //if month is 3, and the date is lesss than 16 it iss still winter
             if (day < 16) {
                 season = "winter";
-            }
             } else {
-                season = "spring";
+                season = "spring"; //else the season will be spring 
             }
-          if (month > 3 && month < 6) {
-                season = "spring";
+        } else if (month > 3 && month < 6) {//if month if bewtween 3 and 6 the season is spring 
+            season = "spring";
 
-            }
-            else if  (month == 6) {
-                if (day < 16) {
-                    season = "spring";
-                }
+        } else if (month == 6) { //if the month is june or 6 , and the day is less than 16 the season is still spring 
+            if (day < 16) {
+                season = "spring";
             } else {
+                season = "summer"; //else the seasin will be summer
+            }
+        } else if (month > 6 && month < 9) {//if the month is between 6 and 9 it is summer
+            season = "summer";
+        } else if (month == 9) { //if the month is 9 and the date is less than 16 it is still summer
+            if (day < 16) {
                 season = "summer";
+            } else {
+                season = "fall"; //else the season will be fall
+            }
+        } else if (month > 9 && month < 12) {//if month is more than 9 and less than 12 the season is fall 
+            season = "fall";
+        } else if (month == 12) { //if the month is 12 and day is less than 16 it is still fall
+            if (day < 16) {
+                season = "fall";
+            } else {
+                season = "winter";//else the season will be winter 
+
+
             }
 
-        
-
-
-
+        }
 
         return season;
     }
@@ -64,9 +74,9 @@ public class A6Q8 {
         System.out.println("What day of the month is it ?");
         //allow user to enter day of month 
         int day = input.nextInt();
-
+        //send infromation to method 
         String currentseason = season(month, day);
-
+        //tell user what season it is 
         System.out.println("The season is " + currentseason);
 
 
--- src/A6Q9.java
+++ src/A6Q9.java
@@ -12,19 +12,40 @@ import java.util.Scanner;
 public class A6Q9 {
 
     public static boolean alldigitsodd(int number) {
+        //while true repeat infinite
+        while (true) {
+            //check to see if the lasat digit of the number is an odd digit 
+            int lastDigit = number % 10;
+            if (lastDigit == 9
+                    || lastDigit == 7
+                    || lastDigit == 5
+                    || lastDigit == 3
+                    || lastDigit == 1) {
+                // get rid of the lsat digit so that the program gcan check the next digit
+                number = number / 10;
 
-        if (number == 9) {
-             || number == 7
-                    || number == 5
-                    || number == 3
-                    || number == 1
-       
-
+            }//check to see if the last digit of the number is a even
+            if (lastDigit == 8
+                    || lastDigit == 6
+                    || lastDigit == 4
+                    || lastDigit == 2) {
+                //if any odd number is found then return false 
+                return false;
+            }
+            //if the number is 0(there is nothing left to check) then break loop
+            if(number==0){
+                break;
+            }
         }
+        //if no even numbers are foudn then return true 
+        return true;
+    }
 
         /**
          * @param args the command line arguments
          */
+    
+
     
 
     public static void main(String[] args) {
@@ -34,9 +55,11 @@ public class A6Q9 {
         System.out.println("Please enter a number ");
         //allow user to enter number 
         int number = input.nextInt();
-
+       //send answer bacak the method
         boolean Num = alldigitsodd(number);
-
-
+        //tell user if it is false 
+        System.out.println(""+Num);
+  
     }
 }
+
