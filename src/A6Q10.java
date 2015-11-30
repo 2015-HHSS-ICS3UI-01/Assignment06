f4d0d496fc40852c7b88046287f43f238f20d4c9
watsk8668 <watsk8668@HRH-IC0027333.wrdsb.ca>
2015-11-30 14:22:54 -0500

    A6 Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8,Q10 done Q9 almost done

--- src/A6Q10.java
+++ src/A6Q10.java
@@ -8,15 +8,21 @@
  * @author watsk8668
  */
 public class A6Q10 {
+    //method that compares the places of dashes
 
     public static String sameDashes(String string1, String string2) {
+        //variable that stores the places of dashes in the first word
         int dashes = string1.indexOf("-");
+        //variable that stores the places of dashes in the second word
         int dashes2 = string2.indexOf("-");
 
+        //if the places of the dashes are the same print out true
         if (dashes == dashes2) {
             System.out.println("True");
             return "True";
-        } else {
+            
+        } // if the places of the dashes are not the same print out false
+        else {
             System.out.println("False");
             return "False";
         }
@@ -26,7 +32,8 @@ public class A6Q10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //
-        sameDashes("abc", "9.8");
+        //run the method
+        //enter two strings
+        sameDashes("abc--", "9.8-gz-");
     }
 }
--- src/A6Q5.java
+++ src/A6Q5.java
@@ -8,18 +8,32 @@
  * @author watsk8668
  */
 public class A6Q5 {
+    //method that randomly prints out 1-5 stars
     public static void chaotic() {
+        //infinite loop
         while (true) {
+         //create a random number from 1-5
          int randNum = (int)(Math.random()*(5 - 1 + 1))+ 1;
+         
+         // if the random number is 1
          if(randNum == 1){
+             //print out 1 star
          System.out.println("*");
-         } else if(randNum == 2){
+         } //if the random number is 2
+         else if(randNum == 2){
+             //print out 2 stars
              System.out.println("**");
-         } else if(randNum == 3) {
+         } //if the random number is 3
+         else if(randNum == 3) {
+             //print out 3 stars
              System.out.println("***");
-         } else if(randNum == 4) {
+         } // if the random number is 4
+         else if(randNum == 4) {
+             //print out 4 stars
              System.out.println("****");
-         } else if(randNum == 5) {
+         } //if the random number is 5
+         else if(randNum == 5) {
+             //print out 5 stars
              System.out.println("*****");
          }
     }
@@ -28,8 +42,7 @@ public class A6Q5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //
-        
+        //run the program
         chaotic();
     }
 }
