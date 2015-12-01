be32e4cb667e3368c7e91d12162cf520f6d233bb
munta <munta@DESKTOP-DQD3ERT>
2015-12-01 13:06:40 -0500

    Final Assignment 03

--- src/A6Q3.java
+++ src/A6Q3.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -11,6 +14,13 @@ public class A6Q3 {
     
     
     public static void factor(int number){
+        //checking every number that is tless then "number"
+        for (int n=1;n<=number;n++){
+            //if the number can divide evernly then it is a factor 
+            if(number% n==0){
+                System.out.println("factors|("+number+")="+n);
+            }
+        }
         
     }
     
@@ -25,6 +35,16 @@ public class A6Q3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //new scnner 
+          Scanner input = new Scanner(System.in);
+          //ask user to enter a number 
+          System.out.println("please enter a number ");
+          //allow user to enter number 
+          int num=input.nextInt();
+          factor(num);
+          
+          
+          
+                  
     }
 }
