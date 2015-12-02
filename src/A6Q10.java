2c3f08fafc6536ecf2a38c5706a0f880df1d8e92
snowc4636 <snowc4636@HRH-IC0027318.wrdsb.ca>
2015-12-02 14:00:56 -0500

    Assignment 6 complete

--- src/A6Q10.java
+++ src/A6Q10.java
@@ -5,33 +5,44 @@ import java.util.Scanner;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author snowc4636
  */
 public class A6Q10 {
-        public static void sameDashes(String one, String two){
+
+    public static void sameDashes(String one, String two) {
+
+        while (true) {
             int first = one.indexOf("-");
             int second = two.indexOf("-");
-            
-            if(first == second){
-                
-                System.out.println(" This statement is true");
-            }else{
-                
-                
-                System.out.println(" This statement is false");
+         
+
+            if (first == second && first >= 0) {
+                one = one.substring(first +1);
+                two = two.substring(second +1);
                
+            }else if(first == -1 && second == -1){
+                System.out.println(" This statement is true");
+                break;
             }
-            
+            else {
+
+
+                System.out.println(" This statement is false");
+                break;
+            }
         }
+
+
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         //
-        
+
         Scanner input = new Scanner(System.in);
         // enter a word
         System.out.println("Please enter a word with at least 1 dash in it ");
@@ -40,6 +51,6 @@ public class A6Q10 {
         System.out.println(" Please enter another word with at least 1 dash in it");
         String two = input.nextLine();
         // formula
-        sameDashes( one, two);
+        sameDashes(one, two);
     }
 }
