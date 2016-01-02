2904b2714b0f0408be51f8bae08b5c6ed3356b55
Peter <Peter@Katrina>
2016-01-01 19:37:48 -0500

    done + comments

--- src/A6Q10.java
+++ src/A6Q10.java
@@ -10,22 +10,22 @@
 public class A6Q10 {
     //method that compares the places of dashes
 
-    public static String sameDashes(String string1, String string2) {
-        //variable that stores the places of dashes in the first word
-        int dashes = string1.indexOf("-");
-        //variable that stores the places of dashes in the second word
-        int dashes2 = string2.indexOf("-");
-
-        //if the places of the dashes are the same print out true
-        while (dashes == dashes2) {
-            System.out.println("True");
-            return "True";
-            
-        } // if the places of the dashes are not the same print out false
-        if (dashes != dashes) {
-            System.out.println("False");
-            return "False";
+    public static String sameDashes(String s1, String s2) {
+        //create variables to save the places to compare
+        int place = 0;
+        //run through all the places in the string in a loop
+        while ((s1.length() > place) && (s2.length() > place)) {
+            //if the spot in the first string has a dash but the second one doesnt -return false
+            if (s1.charAt(place) == '-' && s2.charAt(place) != '-') {
+                return "false";
+              //if th spot in the second string has a dash but the first doesnt -return false
+            } else if (s1.charAt(place) != '-' && s2.charAt(place) == '-') {
+                return "false";
+            }
+            //check the next spot
+            place++;
         }
+        return "true";
     }
 
     /**
@@ -34,6 +34,6 @@ public class A6Q10 {
     public static void main(String[] args) {
         //run the method
         //enter two strings
-        sameDashes("abc--", "9.8-gz-");
+        System.out.println(sameDashes("-abc--", "-9.8--ytfg"));
     }
 }
