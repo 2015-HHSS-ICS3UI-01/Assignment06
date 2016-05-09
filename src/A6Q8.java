829c4719f49670b645e42f4e076906c14f9d03db
tatad6701 <tatad6701@HRH-IC0027335.wrdsb.ca>
2016-05-09 09:10:41 -0400

    Assignment06 (Fifth Commit)

--- src/A6Q8.java
+++ src/A6Q8.java
@@ -16,21 +16,21 @@ public class A6Q8 {
             // If month is 3 and the day is greater than 16, it is Winter
         } else if (month == 3 && day < 16) {
             System.out.println("It is Winter.");
-            // If month is 3, it is Spring
-        } else if (month == 3) {
-            System.out.println("It is Spring.");
             // If month is 12 and day is less than 15, it is Winter
         } else if (month == 12 && day > 15) {
             System.out.println("It is Winter.");
+            // If month is 3, it is Spring
+        } else if (month == 3) {
+            System.out.println("It is Spring.");
             // If month is 12, it is Fall
         } else if (month == 12) {
             System.out.println("It is Fall.");
             // If month is 4 or 5, season is Spring
         } else if (month == 4 || month == 5) {
             System.out.println("It is Spring.");
-            // If month is 6 and day is less than 16, it is Spring
-        } else if (month == 6 && day < 16) {
-            System.out.println("It is Spring.");
+            // If month is 6 and day is less than 16, it is Summer
+        } else if (month == 6 && day <= 16) {
+            System.out.println("It is Summer.");
             // If month is 5 and day is more than 15, it is Summer
         } else if (month == 5 && day > 15) {
             System.out.println("It is Summer.");
@@ -52,6 +52,6 @@ public class A6Q8 {
     public static void main(String[] args) {
         // TODO code application logic here
         // User's input, month and date (< in this order)
-        season(7, 8);
+        season(1, 1);
     }
 }
--- src/A6Q9.java
+++ src/A6Q9.java
@@ -9,11 +9,35 @@
  */
 public class A6Q9 {
 
-   // public static void allDigitsOdd(int ) /**
-   //  * @param args the command line arguments
-    // */
+    public static void allDigitsOdd(int nums) {
+        String number = Integer.toString(nums);
+        boolean allOdd = true;
+        for (int i = 0; i > number.length(); i++) {
+            char v = number.charAt();
+
+        }
+
+    
+        else{ 
+            allOdd = false;
+    }
+    // check if last digit is odd, if it is, then remove digit by dividing by 10
+    if(lastNum == 1 || lastNum == 3 || lastNum == 5 || lastNum
+
+    
+    
+
+== 7){
+}
+}
+
+    /**
+     * @param args the command line arguments
+     */
+    
 
     public static void main(String[] args) {
         // TODO code application logic here
+        allDigitsOdd(343);
     }
 }
