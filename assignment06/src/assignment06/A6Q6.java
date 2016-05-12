3e4d7090c1666698b12f5cdd6d8d6f5a2b299551
fostp4040 <fostp4040@HRH-IC0027333.wrdsb.ca>
2016-05-12 08:24:37 -0400

    ​Wrote a method to print out the last digit of a number

---
+++ assignment06/src/assignment06/A6Q6.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment06;
+
+/**
+ *
+ * @author fostp4040
+ */
+public class A6Q6 {
+    public static void lastDigit(int num){
+        int lastDigit=num%10;
+        System.out.println(lastDigit);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        lastDigit(527);
+    }
+}
