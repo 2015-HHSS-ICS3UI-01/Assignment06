12af4d54e6aa9a83386b6a671c2bfc53b8bd466d
tatad6701 <tatad6701@HRH-IC0027335.wrdsb.ca>
2016-05-13 09:13:06 -0400

    Assignment06 (Eleventh Commit)

--- src/A6Q10.java
+++ src/A6Q10.java
@@ -11,37 +11,20 @@ import java.util.Scanner;
  */
 public class A6Q10 {
 
-    public static String sameDashes1(String string1) {
+    public static String sameDashes(String string1) {
         // New sBuilder
         StringBuilder sBuilder = new StringBuilder();
         // For loop going through each character of the user's input
         for (int i = 0; i < string1.length(); i++) {
             // ADD ANOTHER INTEGER!
-            if (sBuilder.charAt(i) == '-') {
+            if (string1.charAt(i) == '-') {
                 // If both dashes are at the same place then the program will print out 'True'
                 sBuilder.append("True");
             } else {
                 // If not true, then it is automatically false
                 sBuilder.append("False");
             }
-        }
-        // Return the answer back
-        return sBuilder.toString();
-    }
 
-    public static String sameDashes2(String string2) {
-        // New sBuilder
-        StringBuilder sBuilder = new StringBuilder();
-        // For loop going through each character of the user's input
-        for (int i = 0; i < string2.length(); i++) {
-            // ADD ANOTHER INTEGER!
-            if (sBuilder.charAt(i) == '-') {
-                // If both dashes are at the same place then the program will print out 'True'
-                sBuilder.append("True");
-            } else {
-                // If not true, then it is automatically false
-                sBuilder.append("False");
-            }
         }
         // Return the answer back
         return sBuilder.toString();
@@ -63,15 +46,30 @@ public class A6Q10 {
         // Save user's second input to variable 'string2'
         String userInput2 = input.nextLine();
         // Compares both answer together for final result
-        String sameDashes1 = sameDashes1(userInput);
-        String sameDashes2 = sameDashes2(userInput2);
+        String sameDashes1 = sameDashes(userInput);
+        String sameDashes2 = sameDashes(userInput2);
+
+        String leftover;
+        if (sameDashes1.length() > sameDashes2.length()) {
+            leftover = sameDashes1.substring(sameDashes2.length());
+            sameDashes1 = sameDashes1.substring(0, sameDashes2.length());
+        } else {
+            leftover = sameDashes2.substring(sameDashes1.length());
+            sameDashes2 = sameDashes2.substring(0, sameDashes1.length());
+        }
+
         // Compare first string to second
         if (sameDashes1.equals(sameDashes2)) {
-            // If both strings are the same, the program prints 'True'
-            System.out.println("True");
+            if (leftover.length() > 0 && leftover.contains("True")) {
+                System.out.println("False");
+            } else {
+                // If both strings are the same, the program prints 'True'
+                System.out.println("True");
+            }
         } else {
             // If both strings are not the same, the program print 'False'
             System.out.println("False");
+
         }
     }
 }
