ebcd4a7a037090866eb0d123319456cf05aadd3c
halll7908 <halll7908@HRH-IC0027342.wrdsb.ca>
2016-05-13 08:23:39 -0400

    Assignment 6 Finished.

--- src/A6Q10.java
+++ src/A6Q10.java
@@ -8,20 +8,38 @@
  * @author halll7908
  */
 public class A6Q10 {
+    //boolean outputs true or false
     // String 1, String 2
+    public static boolean sameDashes(String s1, String s2) {
+        // get length
+        int lengthS1 = s1.length();
+        int lengthS2 = s2.length();
+        // counter variable
+        int count = 1; // counter variable
+        int start = 0; // variable start
 
-    public static void sameDashes(String s1, String s2) {
-        // String 1
-        StringBuilder sBuilder1 = new StringBuilder(s1);
-        // String 2
-        StringBuilder sBuilder2 = new StringBuilder(s2);
-        for (int i = 0; i < sBuilder1.length(); i++) {
-            if (sBuilder1.charAt(i) == '-' && sBuilder2.charAt(i) == '-') {
-                System.out.println("True");
-            } else if (sBuilder1.charAt(i) != '-' && sBuilder2.charAt(i) != '-') {
-                System.out.println("False.");
+        if (lengthS1 > lengthS2) { // compare lengths
+            count = lengthS1;
+        } else {
+            count = lengthS2;
+        }
+        while (count != start) {
+            //find where dash is in both strings
+            int spot1 = s1.indexOf("-");
+            int spot2 = s2.indexOf("-");
+            if (spot1 == -1 && spot2 == -1) {
+                return true; // return false if not at same spot
+            } else if (spot1 == spot2) {
+                //shorten string to where the first dash in each string was found
+                s1 = s1.substring(spot1 + 1, lengthS1);
+                s2 = s2.substring(spot2 + 1, lengthS2);
+                lengthS1 = lengthS1 - (spot1 + 1);
+                lengthS2 = lengthS2 - (spot2 + 1);
+            } else {
+                return false;
             }
         }
+        return true;
     }
 
     /**
@@ -29,6 +47,7 @@ public class A6Q10 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        sameDashes("cr-a-zy", "bd-a-y");
+        System.out.println(sameDashes("cr-a-zy", "cr-a-z"));
+        System.out.println(sameDashes("c-ra-zy", "cr-a-zy"));
     }
 }
